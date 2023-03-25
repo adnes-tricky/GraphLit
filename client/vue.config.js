d7792d68b3e10cd0a73f152d09b2ebe81abf464d
Adarsh V <84374868+adnes-tricky@users.noreply.github.com>
2023-03-26 03:21:45 +0530

    Adarsh: Added domain url for the deployed server

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
       proxy: {
         'api/': {
-          target: 'https://graph-lit.vercel.app/',
+          target: 'https://graph-lit-server.vercel.app/',
         }
       }
     }
